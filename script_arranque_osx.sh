84ce5e520155db0fd0607ab68483a33b4acfad0c
Ricardo Pérez López <ricpelo@gmail.com>
2013-05-04 12:15:11 +0200

    Retocado script de arranque para Mac OS X

--- script_arranque_osx.sh
+++ script_arranque_osx.sh
@@ -6,9 +6,11 @@
 #    renombramos a Gargoyle
 # 3. Borramos el archivo garglk.ini de Gargoyle.app/Contents/Resource/
 # 4. Copiamos elcirculo.blb y elcirculo.ini a Gargoyle.app/Contents/Resource/
+# 5. Copiamos mplayer2 y su lib/ en Gargoyle.app/Contents/MacOS/
+# EL ARCHIVO .app NO PUEDE TENER ACENTOS
 
 GARGOYLEROOT=$(cd "${0%/*}" && echo $PWD)
-
-cd $GARGOYLEROOT
-$GARGOYLEROOT/Gargoyle.exe $GARGOYLEROOT/../Resources/elcirculo.blb
+cd "$GARGOYLEROOT"
+"$GARGOYLEROOT/mplayer2" -fs "$GARGOYLEROOT/../Resources/The_Ring.avi"
+"$GARGOYLEROOT/Gargoyle.exe" "$GARGOYLEROOT/../Resources/elcirculo.blb"
 
