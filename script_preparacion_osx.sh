e7562e0cb093c1393b6e720a15a30ea40eda7d47
Ricardo Pérez López <ricpelo@gmail.com>
2013-05-05 12:52:00 +0200

    Incluyo script_preparacion_osx.sh

---
+++ script_preparacion_osx.sh
@@ -0,0 +1,7 @@
+mv Gargoyle.app/Contents/MacOS/Gargoyle Gargoyle.app/Contents/MacOS/Gargoyle.exe
+cp script_arranque_osx.sh Gargoyle.app/Contents/MacOS/Gargoyle
+cp ~/Downloads/The.Ring.La.Señal.DVDRip.Spanish.by.jurimu.avi Gargoyle.app/Contents/Resources/pepe.avi
+cp ~/Downloads/mplayer2.app/Contents/MacOS/mplayer2 Gargoyle.app/Contents/Resources/
+cp -r ~/Downloads/mplayer2.app/Contents/MacOS/lib Gargoyle.app/Contents/Resources/
+cp ~/Dropbox/Public/elcirculo.* Gargoyle.app/Contents/Resources/
+
